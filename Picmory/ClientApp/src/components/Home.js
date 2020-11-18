364eae64d95ec4572e914035c815f63603d4b795
Sophie <szaniszlozs7@gmail.com>
2020-11-18 14:28:21 +0000

    add home page, home css

--- Picmory/ClientApp/src/components/Home.js
+++ Picmory/ClientApp/src/components/Home.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import "./Home.css";
+import name from './transparentNameOnly.png';
 
 export class Home extends Component {
   static displayName = Home.name;
 
   render () {
     return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+      <div className="home">
+            <h1>Welcome on</h1>
+            <img src={name} className="home-name" />
+            <h3>Save and share your memories by photos</h3>
+            <h2>
+                <Link tag={ Link } to="/fetch-data">Sign up</Link> or <Link tag={Link} to="/counter">Log in</Link> to start
+            </h2>
       </div>
     );
   }
