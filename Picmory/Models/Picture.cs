a06fb96eb32e6fc5f35cd30eb2ae05f9e9b8b232
mm2005 <55625812+mm2005@users.noreply.github.com>
2020-12-08 13:36:22 +0100

    Started Models

---
+++ Picmory/Models/Picture.cs
@@ -0,0 +1,14 @@
+﻿using Picmory.Util;
+
+namespace Picmory.Models
+{
+    public class Picture
+    {
+        public string Path { get; set; }
+        public string Description { get; set; }
+        public AccessType Access { get; set; }
+        public string Type { get; set; }
+        public User Owner { get; set; }
+        public string FolderName { get; set; }
+    }
+}
---
+++ Picmory/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using Picmory.Util;
+
+namespace Picmory.Models
+{
+    public class User
+    {
+        private string Name { get; set; }
+        private string EMail { get; set; }
+        private string Password { get; set; }
+        private Theme Theme { get; set; }
+        private string ProfilePicture { get; set; }
+
+
+    }
+}
