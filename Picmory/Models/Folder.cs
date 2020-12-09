b77816908110b11ef3a6249ea83ec18cc1c0518f
mm2005 <55625812+mm2005@users.noreply.github.com>
2020-12-09 13:49:33 +0100

    Added basic Models

--- Picmory/Models/Folder.cs
+++ Picmory/Models/Folder.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Picmory.Util;
 
 namespace Picmory.Models
 {
     public class Folder
     {
+        public User Owner { get; set; }
+        public string FolderName { get; set; }
+        public AccessType Access { get; set; }
     }
 }
