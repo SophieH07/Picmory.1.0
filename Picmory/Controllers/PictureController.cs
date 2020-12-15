e8d17dea464227114611038607b861838d090b71
mm2005 <55625812+mm2005@users.noreply.github.com>
2020-12-15 09:43:25 +0100

    PictureController created

---
+++ Picmory/Controllers/PictureController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Picmory.Models;
+using Picmory.Models.Repositorys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Picmory.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class PictureController :ControllerBase
+    {
+        private readonly IUserRepository userRepository;
+        private readonly IFolderRepository folderRepository;
+        private IConfiguration _config;
+
+        public PictureController(IUserRepository userRepository, IFolderRepository folderRepository, IConfiguration config)
+        {
+            this.userRepository = userRepository;
+            this.folderRepository = folderRepository;
+            _config = config;
+        }
+
+
+        [HttpGet("picture")]
+        public string Info(int pictureId)
+        {
+            string result = null;
+            var currentUser = HttpContext.User;
+            if (currentUser.HasClaim(c => c.Type == "Id"))
+            {
+                int id = int.Parse(currentUser.Claims.FirstOrDefault(c => c.Type == "Id").Value);
+                User user = userRepository.GetUserData(id);
+            }
+            return result;
+        }
+
+    }
+}
