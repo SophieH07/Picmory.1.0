e3a705599f2735c12a2159ff8a866e08d1f69412
SophieH07 <53736936+SophieH07@users.noreply.github.com>
2021-01-04 18:52:01 +0100

    add basic skeleton for profile site

--- Picmory/ClientApp/src/App.js
+++ Picmory/ClientApp/src/App.js
@@ -4,19 +4,20 @@ import { Layout } from './components/Layout/Layout';
 import { Home } from './components/Home/Home';
 import { Register } from './components/Register/Register';
 import { Login } from './components/Login/Login';
-
+import { Profile } from './components/Profile/Profile';
 import './custom.css'
 
 export default class App extends Component {
-  static displayName = App.name;
+    static displayName = App.name;
 
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/login' component={Login} />
-        <Route path='/register' component={Register} />
-      </Layout>
-    );
-  }
+    render() {
+        return (
+            <Layout>
+                <Route exact path='/' component={Home} />
+                <Route path='/login' component={Login} />
+                <Route path='/register' component={Register} />
+                <Route path='/profile' component={Profile} />
+            </Layout>
+        );
+    }
 }
