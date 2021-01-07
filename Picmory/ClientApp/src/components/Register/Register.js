634473d35bf162275467fce0d4c8bb490b3e1fdc
SophieH07 <53736936+SophieH07@users.noreply.github.com>
2021-01-07 17:23:10 +0100

    add password visibility to registration

--- Picmory/ClientApp/src/components/Register/Register.js
+++ Picmory/ClientApp/src/components/Register/Register.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import "./Register.css";
+import eye from '../../img/eye.png';
+import PasswordMask from 'react-password-mask';
 
 export class Register extends Component {
     static displayName = Register.name;
@@ -8,9 +10,21 @@ export class Register extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isDisabled: true
+            isDisabled: true,
+            hidden1: true,
+            hidden2: true
         }
+        this.toggleShowOne = this.toggleShowOne.bind(this);
+        this.toggleShowTwo = this.toggleShowTwo.bind(this);
         //this.submitForm = this.submitForm.bind(this);
+    }
+
+    toggleShowOne() {
+        this.setState({ hidden1: !this.state.hidden1 });
+    }
+
+    toggleShowTwo() {
+        this.setState({ hidden2: !this.state.hidden2 });
     }
 
     validateEmail(email) {
@@ -26,6 +40,10 @@ export class Register extends Component {
                 emailError: true
             })
         }
+    }
+
+    validatePassword(password1, password2) {
+
     }
 
     handleChange(e) {
@@ -47,11 +65,21 @@ export class Register extends Component {
             <div className="register">
                 <h2>Create Account</h2>
                 <div className="input-fields">
-                    <input id="name" placeholder="Your username" type="text" />
-                    {this.state.emailError ? <span className="warning">Please Enter valid email address</span> : ''}
-                    <input id="email" name="email" placeholder="Your email" type="text" onChange={(e) => { this.handleChange(e) }} />
-                    <input id="password1" placeholder="Your password" type="text" />
-                    <input id="password2" placeholder="Repeat your password" type="text" />
+                    <div>
+                        <input id="name" placeholder="Your username" type="text" />
+                    </div>
+                    <div>
+                        {this.state.emailError ? <p><span className="warning">Please Enter valid email address</span></p> : ''}
+                        <input id="email" name="email" placeholder="Your email" type="text" onChange={(e) => { this.handleChange(e) }} />
+                    </div>
+                    <div className="password-container">
+                        <input id="password1" type={ this.state.hidden1 ? "password" : "text" } placeholder="Your password"/>
+                        <img src={eye} className="eye" onClick={this.toggleShowOne} />
+                    </div>
+                    <div className="password-container">
+                        <input id="password2" type={this.state.hidden2 ? "password" : "text"}  placeholder="Repeat your password"/>
+                        <img src={eye} className="eye" onClick={this.toggleShowTwo} />
+                    </div>
                 </div>
                 <div>
                     <p>
@@ -59,7 +87,7 @@ export class Register extends Component {
                          I agree all statements in Terms of service.
                         </p>
                 </div>
-                <button>Sign up</button>
+                <button disabled={this.state.isDisabled}>Sign up</button>
                 <p>Already have an account? Yay! <Link tag={Link} to="/login"> Login here</Link></p>
             </div>
         );
