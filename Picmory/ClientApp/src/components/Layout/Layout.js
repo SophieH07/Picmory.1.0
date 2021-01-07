15912760b57dc88ee719da056582fa6d9d2738df
SophieH07 <53736936+SophieH07@users.noreply.github.com>
2021-01-07 14:33:18 +0100

    style register div and container

--- Picmory/ClientApp/src/components/Layout/Layout.js
+++ Picmory/ClientApp/src/components/Layout/Layout.js
@@ -3,16 +3,21 @@ import { Container } from 'reactstrap';
 import { NavMenu } from '../../NavMenu/NavMenu';
 
 export class Layout extends Component {
-  static displayName = Layout.name;
+    static displayName = Layout.name;
 
-  render () {
-    return (
-      <div>
-        <NavMenu />
-        <Container>
-          {this.props.children}
-        </Container>
-      </div>
-    );
-  }
+    render() {
+        const containerStyle = {
+            textAlign: '-webkit-center'
+        };
+
+        return (
+
+            <div>
+                <NavMenu />
+                <Container style={containerStyle}>
+                    {this.props.children}
+                </Container >
+            </div >
+        );
+    }
 }
--- Picmory/ClientApp/src/components/Register/Register.js
+++ Picmory/ClientApp/src/components/Register/Register.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import "./Register.css";
 
 export class Register extends Component {
-  static displayName = Register.name;
-  render() {
-    return (
-      <div>
-        <h2>Create Account</h2>
-        <p>Your name</p>
-        <input type="text"></input>
-        <p>Your Email</p>
-        <input type="text"></input>
-        <p>Password</p>
-        <input type="text"></input>
-        <p>Repeat your passord</p>
-        <input type="text"></input>
-        <br></br>
-        <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"></input>
-        <p>I agree all statenments in Terms of service</p>
-        <button>Sign up</button>
-        <p>Have already an account? Login here</p>
-      </div>
-    );
-  }
+    static displayName = Register.name;
+    render() {
+        return (
+            <div className="register">
+                <h2>Create Account</h2>
+                <div className="input-fields">
+                    <input id="name" placeholder="Your username" type="text" />
+                    <input id="email" placeholder="Your email" type="text" />
+                    <input id="password1" placeholder="Your password" type="text" />
+                    <input id="password2" placeholder="Repeat your password" type="text" />
+                </div>
+                <div>
+                    <p>
+                        <input type="checkbox" id="" name="" value="" />
+                         I agree all statements in Terms of service.
+                        </p>
+                </div>
+                <button>Sign up</button>
+                <p>Already have an account? Yay! <Link tag={Link} to="/login"> Login here</Link></p>
+            </div>
+        );
+    }
 }
