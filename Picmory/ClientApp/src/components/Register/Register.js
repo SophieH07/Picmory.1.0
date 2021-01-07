52bad5936293d2bff9510ca6242e130601da0e95
SophieH07 <53736936+SophieH07@users.noreply.github.com>
2021-01-07 18:15:45 +0100

    add password validation and check for both passwords are equal

--- Picmory/ClientApp/src/components/Register/Register.js
+++ Picmory/ClientApp/src/components/Register/Register.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import "./Register.css";
 import eye from '../../img/eye.png';
-import PasswordMask from 'react-password-mask';
 
 export class Register extends Component {
     static displayName = Register.name;
@@ -12,19 +11,18 @@ export class Register extends Component {
         this.state = {
             isDisabled: true,
             hidden1: true,
-            hidden2: true
+            hidden2: true,
         }
-        this.toggleShowOne = this.toggleShowOne.bind(this);
-        this.toggleShowTwo = this.toggleShowTwo.bind(this);
+        this.toggleShow = this.toggleShow.bind(this);
         //this.submitForm = this.submitForm.bind(this);
     }
 
-    toggleShowOne() {
-        this.setState({ hidden1: !this.state.hidden1 });
-    }
-
-    toggleShowTwo() {
-        this.setState({ hidden2: !this.state.hidden2 });
+    toggleShow(e) {
+        if (e.target.name == "password1") {
+            this.setState({ hidden1: !this.state.hidden1 });
+        } else {
+            this.setState({ hidden2: !this.state.hidden2 });
+        }
     }
 
     validateEmail(email) {
@@ -42,22 +40,45 @@ export class Register extends Component {
         }
     }
 
-    validatePassword(password1, password2) {
+    validatePassword(password) {
+        const pattern = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{6,})");
+        const validInput = pattern.test(password);
+        if (validInput === true) {
+            this.setState({
+                passwordError: false,
+                password: password
+            })
+        } else {
+            this.setState({
+                passwordError: true
+            })
+        }
+    }
 
+    checkIfPasswordsAreEqual(confirmPassword) {
+        if (this.state.password === confirmPassword) {
+            this.setState({
+                equalPasswords: false
+            })
+        } else {
+            this.setState({
+                equalPasswords: true
+            })
+        }
     }
 
     handleChange(e) {
-        //const target = e.target;
-        //const name = target.name;
-
-        //this.setState({
-        //    [name]: value
-        //});
-
         if (e.target.name === 'email') {
             this.validateEmail(e.target.value);
         }
 
+        if (e.target.name === 'password1') {
+            this.validatePassword(e.target.value);
+        }
+
+        if (e.target.name === 'password2') {
+            this.checkIfPasswordsAreEqual(e.target.value);
+        }
     }
 
     render() {
@@ -72,13 +93,15 @@ export class Register extends Component {
                         {this.state.emailError ? <p><span className="warning">Please Enter valid email address</span></p> : ''}
                         <input id="email" name="email" placeholder="Your email" type="text" onChange={(e) => { this.handleChange(e) }} />
                     </div>
+                    {this.state.passwordError ? <p><span className="warning">The password must be at least 6 char long, contain a lowercase letter, an uppercase letter and a number</span></p> : ''}
                     <div className="password-container">
-                        <input id="password1" type={ this.state.hidden1 ? "password" : "text" } placeholder="Your password"/>
-                        <img src={eye} className="eye" onClick={this.toggleShowOne} />
+                        <input name="password1" type={this.state.hidden1 ? "password" : "text"} placeholder="Your password" onChange={(e) => { this.handleChange(e) }} />
+                        <img name="password1" src={eye} className="eye" onClick={this.toggleShow} />
                     </div>
+                    {this.state.equalPasswords ? <p><span className="warning">The passwords are not equal</span></p> : ''}
                     <div className="password-container">
-                        <input id="password2" type={this.state.hidden2 ? "password" : "text"}  placeholder="Repeat your password"/>
-                        <img src={eye} className="eye" onClick={this.toggleShowTwo} />
+                        <input name="password2" type={this.state.hidden2 ? "password" : "text"} placeholder="Repeat your password" onChange={(e) => { this.handleChange(e) }} />
+                        <img name="password2" src={eye} className="eye" onClick={this.toggleShow} />
                     </div>
                 </div>
                 <div>
