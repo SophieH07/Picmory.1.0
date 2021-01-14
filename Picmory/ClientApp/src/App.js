d9fc83ecaec0e1261bb921ed39f336b71f629b1e
SophieH07 <53736936+SophieH07@users.noreply.github.com>
2021-01-14 12:47:34 +0100

    rewrite home, navmenu and profile

--- Picmory/ClientApp/src/App.js
+++ Picmory/ClientApp/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route } from 'react-router';
-import { Layout } from './components/Layout/Layout';
 import { NavMenu } from './components/NavMenu/NavMenu';
 import { Home } from './components/Home/Home';
 import { Register } from './components/Register/Register';
@@ -9,11 +8,8 @@ import { Profile } from './components/Profile/Profile';
 import './custom.css'
 
 function App() {
-    const containerStyle = {
-        textAlign: '-webkit-center'
-    };
     return (
-        <div style={containerStyle}>
+        <div className="main">
             <NavMenu />
             <Route exact path='/' component={Home} />
             <Route path='/login' component={Login} />
--- Picmory/ClientApp/src/components/Profile/Profile.js
+++ Picmory/ClientApp/src/components/Profile/Profile.js
@@ -1,49 +1,30 @@
-﻿import React, { Component } from "react";
+﻿import React  from "react";
 import { Link } from "react-router-dom";
-import axios from 'axios';
 import pencil from "../../img/pngwing.com.png";
 import "./Profile.css";
 
-export class Profile extends Component {
-    static displayName = Profile.name;
-
-    componentDidMount() {
-        axios.get("/user/myinfo").then(result => {
-            console.log(result);
-            this.setState({
-                username: result.data.username,
-                profilePicture: result.data.profilePicture,
-                followers: result.data.followers,
-                following: result.data.following,
-                folders: result.data.folders,
-                pictures: result.data.pictures
-            })
-        })
-    }
-
-    render() {
-        return (
-            <div className="profile">
-                <div className="left-side">
-                    <img className="profile-pic" alt="profile pic" />
-                    <p className="username">username here</p>
-                    <p>0 followers 0 following</p>
-                    <div>
-                        <div className="upload-pic">
-                            <Link to="/">upload picture +</Link>
-                        </div>
-                    </div>
-                    <div className="folders">
-                        <div className="folder">
-                            <Link to="/">folder<img className="pencil" src={pencil} alt="edit" /></Link>
-                        </div>
-                        <div className="folder new"><Link to="/">new folder +</Link></div>
+export function Profile() {
+    return (
+        <div className="profile" >
+            <div className="left-side">
+                <img className="profile-pic" alt="profile pic" />
+                <p className="username">username here</p>
+                <p>0 followers 0 following</p>
+                <div>
+                    <div className="upload-pic">
+                        <Link to="/">upload picture +</Link>
                     </div>
                 </div>
-                <div className="right-side">
-                    <p>basically all the pictures here</p>
+                <div className="folders">
+                    <div className="folder">
+                        <Link to="/">folder<img className="pencil" src={pencil} alt="edit" /></Link>
+                    </div>
+                    <div className="folder new"><Link to="/">new folder +</Link></div>
                 </div>
             </div>
-        );
-    }
+            <div className="right-side">
+                <p>basically all the pictures here</p>
+            </div>
+        </div>
+    );
 }
